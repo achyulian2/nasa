5c355110653e7e14d1d4804d00ea9a0b4ad900c3
achyulian2 <101523681+achyulian2@users.noreply.github.com>
2022-03-14 03:13:27 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=XOmFJ6US1RZsVNbtnzivglVvYpShpvSc6GOr4Luz";
         
         $("#loadImage").click(function(){
             $.ajax({
